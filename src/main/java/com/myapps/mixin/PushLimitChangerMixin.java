71bc2c20cfee3b6303cdc20008d4904e7ebc457c
zayoon0o0 <zainn.abdaqqa@gmail.com>
2026-09-08 14:35:46 +0300

    fix again x7

--- src/main/java/com/myapps/mixin/PushLimitChangerMixin.java
+++ src/main/java/com/myapps/mixin/PushLimitChangerMixin.java
@@ -5,8 +5,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
-@Mixin(value = PistonStructureResolver.class, priority = 2000) // because sOmEoThEr modes have similer features and uh they all have tiny limits which arent int limit
-public class ExampleMixin {
+@Mixin(value = PistonStructureResolver.class, priority = 2000) // because sOmEoThEr mods have similar features and uh they all have tiny limits which arent int limit
+public class PushLimitChangerMixin {
 
 	@ModifyExpressionValue(
 			method = "addBlockLine",
