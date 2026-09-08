9ec545bda82c7ff144fe2fe750b0852e7fa2c188
zayoon0o0 <zainn.abdaqqa@gmail.com>
2026-09-08 14:23:49 +0300

    fix again x5

--- src/main/java/com/myapps/mixin/ExampleMixin.java
+++ src/main/java/com/myapps/mixin/ExampleMixin.java
@@ -2,16 +2,20 @@ package com.myapps.mixin;
 
 import net.minecraft.world.level.block.piston.PistonStructureResolver;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
-@Mixin(value = PistonStructureResolver.class, priority = 2000)
-public class ExampleMixin
-{
-	@ModifyConstant(method = "addBlockLine", constant = @Constant(intValue = PistonStructureResolver.MAX_PUSH_DEPTH), expect = 3)
-	private int pushLimit(int original)
-	{
+@Mixin(value = PistonStructureResolver.class, priority = 2000) // because sOmEoThEr modes have similer features and uh they all have tiny limits which arent int limit
+public class ExampleMixin {
+
+	@ModifyExpressionValue(
+			method = "addBlockLine",
+			at = @At(
+					value = "CONSTANT",
+					args = "intValue=12"
+			)
+	)
+	private int pushLimit(int original) {
 		return Integer.MAX_VALUE; // yes.
 	}
 }
-
